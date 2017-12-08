f5fbda0476a9c83aa725c392ec56e9537c6d2513
yavuzsel1m <32516182+yavuzsel1m@users.noreply.github.com>
2017-12-08 19:18:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'kürt') {
+    	message.reply('Ne! Birisi kürt mü dedi?');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
